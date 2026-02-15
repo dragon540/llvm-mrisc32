1f9f2def04be7bcc8f4df5182088b224ea60e008
Shobhit <shobhit32t@gmail.com>
2026-02-16 00:52:14 +0530

    Basic code generation for function prologue and epilogue.
    TODO: Refine it further and verify

--- llvm/lib/Target/MRISC32/MRISC32FrameLowering.cpp
+++ llvm/lib/Target/MRISC32/MRISC32FrameLowering.cpp
@@ -4,14 +4,79 @@
 
 #include "MRISC32FrameLowering.h"
 
+#include "MRISC32InstrInfo.h"
+#include "MRISC32Subtarget.h"
+
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
+#include "llvm/Support/Alignment.h"
+#include "llvm/IR/DebugLoc.h"
+
+#define GET_INSTRINFO_ENUM
+#include "MRISC32GenInstrInfo.inc"
+
+#define GET_REGINFO_ENUM
+#include "MRISC32GenRegisterInfo.inc"
+
 using namespace llvm;
 
-void MRISC32FrameLowering::emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const {
+MRISC32FrameLowering::MRISC32FrameLowering(const MRISC32Subtarget &STI)
+   : TargetFrameLowering(StackGrowsDown, Align(8), -4),
+     STI(STI),
+     TII(*STI.getInstrInfo()),
+     TRI(STI.getRegisterInfo()) {}
 
+void MRISC32FrameLowering::emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const {
+    MachineBasicBlock::iterator MBBI = MBB.begin();
+    MachineFrameInfo &MFI = MF.getFrameInfo();
+    uint64_t stackSize = MFI.getStackSize();
+
+    DebugLoc DL;
+
+    Register framePointer = MRISC32::r28;
+    Register stackPointer = MRISC32::r29;
+
+    BuildMI(MBB, MBBI, DL, TII.get(MRISC32::ADD_C))
+        .addReg(stackPointer, RegState::Kill)
+        .addReg(stackPointer, RegState::Kill)
+        .addImm(-stackSize)
+        .setMIFlag(MachineInstr::FrameSetup);
+
+    BuildMI(MBB, MBBI, DL, TII.get(MRISC32::STW_C))
+        .addReg(framePointer, RegState::Kill)
+        .addReg(stackPointer, RegState::Kill)
+        .addImm(stackSize - 4);
+
+    BuildMI(MBB, MBBI, DL, TII.get(MRISC32::ADD_C))
+        .addReg(framePointer)
+        .addReg(stackPointer)
+        .addImm(stackSize);
 }
   
 void MRISC32FrameLowering::emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const {
+    MachineBasicBlock::iterator MBBI = MBB.getFirstTerminator();
+    MachineFrameInfo &MFI = MF.getFrameInfo();
+    uint64_t stackSize = MFI.getStackSize();
     
+    DebugLoc DL;
+
+    Register framePointer = MRISC32::r28;
+    Register stackPointer = MRISC32::r29;
+
+    BuildMI(MBB, MBBI, DL, TII.get(MRISC32::LDW_C))
+        .addReg(framePointer, RegState::Kill)
+        .addReg(stackPointer, RegState::Kill)
+        .addImm(stackSize - 4);
+
+    BuildMI(MBB, MBBI, DL, TII.get(MRISC32::ADD_C))
+        .addReg(stackPointer)
+        .addReg(stackPointer)
+        .addImm(stackSize);
+
 }
 
 bool MRISC32FrameLowering::hasFPImpl(const MachineFunction &MF) const {
--- llvm/lib/Target/MRISC32/MRISC32FrameLowering.h
+++ llvm/lib/Target/MRISC32/MRISC32FrameLowering.h
@@ -5,16 +5,25 @@
 #ifndef LLVM_LIB_TARGET_MRISC32_MRISC32FRAMELOWERING_H
 #define LLVM_LIB_TARGET_MRISC32_MRISC32FRAMELOWERING_H
 
+#include "MRISC32.h"
+
 #include "llvm/CodeGen/TargetFrameLowering.h"
 
 namespace llvm
 {
+class MachineInstrBuilder;
+class MCCFIInstruction;
+class MRISC32Subtarget;
+class MRISC32RegisterInfo;
 class MRISC32Subtarget;
 
 class MRISC32FrameLowering : public TargetFrameLowering {
 public:
-    explicit MRISC32FrameLowering(const MRISC32Subtarget &sti)
-        : TargetFrameLowering(TargetFrameLowering::StackGrowsDown, Align(8), 0) {}
+    const MRISC32Subtarget &STI;
+    const TargetInstrInfo &TII;
+    const MRISC32RegisterInfo *TRI;
+
+    explicit MRISC32FrameLowering(const MRISC32Subtarget &STI);
 
     void emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
   
