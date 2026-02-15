ac1656ac012fd05747be68ac2fa7bc6c2065a34b
Shobhit <shobhit32t@gmail.com>
2026-02-16 00:51:08 +0530

    Add Load word instruction and fix legalization

--- llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.cpp
+++ llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.cpp
@@ -4,9 +4,11 @@
 using namespace llvm;
 
 MRISC32LegalizerInfo::MRISC32LegalizerInfo(const MRISC32Subtarget &ST) :
-    LegalizerInfo() {
+    LegalizerInfo() 
+    {
         getLegacyLegalizerInfo().computeTables();
-        // Allow constants, additions, and returns
+
         getActionDefinitionsBuilder(TargetOpcode::G_CONSTANT).legalFor({LLT::scalar(32)});
         getActionDefinitionsBuilder(TargetOpcode::G_ADD).legalFor({LLT::scalar(32)});
+        getActionDefinitionsBuilder(TargetOpcode::G_STORE).legalFor({{LLT::scalar(32), LLT::pointer(0, 32)}});
     }
