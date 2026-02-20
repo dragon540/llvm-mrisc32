e12d18233e671964815f4cd6645fcf914bbe8cc5
Shobhit <shobhit32t@gmail.com>
2026-02-21 03:00:14 +0530

    Extend instruction support to SUB, MUL and DIV instructions.

--- llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.cpp
+++ llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.cpp
@@ -10,5 +10,8 @@ MRISC32LegalizerInfo::MRISC32LegalizerInfo(const MRISC32Subtarget &ST) :
 
         getActionDefinitionsBuilder(TargetOpcode::G_CONSTANT).legalFor({LLT::scalar(32)});
         getActionDefinitionsBuilder(TargetOpcode::G_ADD).legalFor({LLT::scalar(32)});
+        getActionDefinitionsBuilder(TargetOpcode::G_SUB).legalFor({LLT::scalar(32)});
+        getActionDefinitionsBuilder(TargetOpcode::G_MUL).legalFor({LLT::scalar(32)});
+        getActionDefinitionsBuilder(TargetOpcode::G_SDIV).legalFor({LLT::scalar(32)});
         getActionDefinitionsBuilder(TargetOpcode::G_STORE).legalFor({{LLT::scalar(32), LLT::pointer(0, 32)}});
     }
