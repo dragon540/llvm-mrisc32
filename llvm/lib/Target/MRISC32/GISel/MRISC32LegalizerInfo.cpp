c84a28770663f2efc576c2e0694c36b40aa62d0f
Shobhit <shobhit32t@gmail.com>
2026-03-29 17:08:47 +0530

    Custom legalize 64 bits scalar add

--- llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.cpp
+++ llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.cpp
@@ -1,6 +1,8 @@
 #include "MRISC32LegalizerInfo.h"
 #include "MRISC32Subtarget.h"
 
+#include "llvm/CodeGen/GlobalISel/LegalizerHelper.h"
+
 using namespace llvm;
 
 MRISC32LegalizerInfo::MRISC32LegalizerInfo(const MRISC32Subtarget &ST) :
@@ -25,5 +27,63 @@ MRISC32LegalizerInfo::MRISC32LegalizerInfo(const MRISC32Subtarget &ST) :
         {LLT::scalar(32), LLT::pointer(0, 32), LLT::scalar(16), 2}, // 16-bit load/store
         {LLT::scalar(32), LLT::pointer(0, 32), LLT::scalar(8),  1}  // 8-bit load/store
         });
-        // Add this to your LegalizerInfo constructor
+        
+        // Handle G_ADD with scalar 64 using custom legalization
+        getActionDefinitionsBuilder(TargetOpcode::G_ADD).customFor({LLT::scalar(64)});
     }
+
+bool MRISC32LegalizerInfo::legalizeCustom(LegalizerHelper &Helper, MachineInstr &MI, 
+                                            LostDebugLocObserver &LocObserver) const 
+{
+    MachineIRBuilder &MIRBuilder = Helper.MIRBuilder;
+    MachineRegisterInfo &MRI = *MIRBuilder.getMRI();
+    GISelChangeObserver &Observer = Helper.Observer;
+    switch (MI.getOpcode()) {
+        case TargetOpcode::G_ADD:
+        return legalizeAdd(MI, MRI, MIRBuilder, Observer);
+    }
+    llvm_unreachable("expected switch to return");
+}
+
+bool MRISC32LegalizerInfo::legalizeAdd(MachineInstr &MI, MachineRegisterInfo &MRI, 
+                    MachineIRBuilder &MIRBuilder, GISelChangeObserver &Observer) const 
+{
+    Register Op1 = MI.getOperand(1).getReg();
+    Register Op2 = MI.getOperand(2).getReg();
+
+    LLT scalar32 = LLT::scalar(32);
+    LLT scalar1  = LLT::scalar(1);
+
+    auto unmergeOp1 = MIRBuilder.buildUnmerge(scalar32, Op1);
+    auto unmergeOp2 = MIRBuilder.buildUnmerge(scalar32, Op2);
+
+    Register lowBitsOp1 = unmergeOp1.getReg(0);
+    Register highBitsOp1 = unmergeOp1.getReg(1);
+
+    Register lowBitsOp2 = unmergeOp2.getReg(0);
+    Register highBitsOp2 = unmergeOp2.getReg(1);
+
+    Register lowBitsResult = MRI.createVirtualRegister(&MRISC32::GPRRegClass);
+    Register highBitsResult = MRI.createVirtualRegister(&MRISC32::GPRRegClass);
+    Register carry = MRI.createGenericVirtualRegister(scalar1);
+    Register carry32 = MRI.createGenericVirtualRegister(scalar32);
+
+    MIRBuilder.buildAdd(lowBitsResult, lowBitsOp1, lowBitsOp2);
+
+    // carry = (lowBitsResult < low1BitsOp1)
+    // NOTE: ICMP_ULT is not yet legalized by the MRISC32 backend
+    MIRBuilder.buildICmp(CmpInst::ICMP_ULT, carry, lowBitsResult, lowBitsOp1);
+
+    // extend carry to 32 bits
+    MIRBuilder.buildZExt(carry32, carry);
+
+    MIRBuilder.buildAdd(highBitsResult, highBitsOp1, highBitsOp2);
+    MIRBuilder.buildAdd(highBitsResult, highBitsResult, carry32);
+
+    // combine into a 64-bit result
+    Register ResultReg = MI.getOperand(0).getReg();
+    MIRBuilder.buildMergeValues(ResultReg, {lowBitsResult, highBitsResult});
+
+    MI.eraseFromParent();
+    return true;
+}
--- llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.h
+++ llvm/lib/Target/MRISC32/GISel/MRISC32LegalizerInfo.h
@@ -1,4 +1,6 @@
 #include "llvm/CodeGen/GlobalISel/LegalizerInfo.h"
+#include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
+#include "llvm/CodeGen/GlobalISel/GISelChangeObserver.h"
 
 namespace llvm {
 
@@ -7,5 +9,8 @@ class MRISC32Subtarget;
 class MRISC32LegalizerInfo : public LegalizerInfo {
 public:
   MRISC32LegalizerInfo(const MRISC32Subtarget &ST);
+
+  bool legalizeCustom(LegalizerHelper &Helper, MachineInstr &MI, LostDebugLocObserver &LocObserver) const override;
+  bool legalizeAdd(MachineInstr &MI, MachineRegisterInfo &MRI, MachineIRBuilder &MIRBuilder, GISelChangeObserver &Observer) const;
 };
 } // end namespace llvm
